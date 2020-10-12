ed556fa02ae67a4964e77e9356ab2f499df87587
xiongshuo <xiongshuor2d@tju.edu.cn>
2020-10-12 16:16:37 +0800

    delet build files

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/media/allen/SSD/Robotaxi_tju/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/allen/robotaxi/devel;/opt/ros/melodic'.split(';') if '/home/allen/robotaxi/devel;/opt/ros/melodic' != '' else []
