4fdd6644d88c1c94d6cbab22f89400acfc3ed796
xiongshuo <xiongshuor2d@tju.edu.cn>
2020-10-12 16:06:25 +0800

    add ros test proj

---
+++ build/beginner_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+  "/media/allen/SSD/Robotaxi_tju/devel/lib/beginner_tutorials/talker.pdb"
+  "/media/allen/SSD/Robotaxi_tju/devel/lib/beginner_tutorials/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/media/allen/SSD/Robotaxi_tju/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/allen/robotaxi/devel;/opt/ros/melodic'.split(';') if '/home/allen/robotaxi/devel;/opt/ros/melodic' != '' else []
